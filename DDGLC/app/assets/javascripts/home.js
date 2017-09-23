5679a6382a935aff98534e37688687a6dc42b737
macksimiljan <macksimiljan@users.noreply.github.com>
2017-09-23 08:34:55 +0200

    added smooth scrolling to home

---
+++ DDGLC/app/assets/javascripts/home.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.scroll-button').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 10
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
