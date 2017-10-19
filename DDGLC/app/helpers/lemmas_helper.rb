eca8e6c242af143d7e897d67a038a2721a2282b5
macksimiljan <macksimiljan@users.noreply.github.com>
2017-10-19 21:13:12 +0200

    created comment button helper

--- DDGLC/app/helpers/lemmas_helper.rb
+++ DDGLC/app/helpers/lemmas_helper.rb
@@ -3,4 +3,10 @@ module LemmasHelper
     return '-' if field.blank?
     field
   end
+
+  def comment_button(target)
+    content_tag :button, class: 'btn btn-default btn-add-comment', type: 'button', 'data-toggle': 'collapse', 'data-target':target do
+      fa_icon('commenting-o')
+    end
+  end
 end
