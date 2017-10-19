43473dede4ad26ee3e081a025cca2c6ed53b5aa1
macksimiljan <macksimiljan@users.noreply.github.com>
2017-10-19 22:37:39 +0200

    adapted migrating existing data

--- DDGLC/app/models/lemma_comment.rb
+++ DDGLC/app/models/lemma_comment.rb
@@ -3,6 +3,9 @@ class LemmaComment < ApplicationRecord
   belongs_to :created_by, class_name: 'User'
   belongs_to :updated_by, class_name: 'User'
 
+  validates :content, presence: true, length: { in: 4..650 }
+  validates :created_by, presence: true
+
   def self.of(lemma_id, field)
     LemmaComment.where(lemma_id: lemma_id, field: field)
   end
