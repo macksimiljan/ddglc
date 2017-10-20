3e38787738f7d8f916a28b029f10e6bc1c08e1c4
macksimiljan <macksimiljan@users.noreply.github.com>
2017-10-20 13:57:43 +0200

    refactored comments and show lemma

--- DDGLC/app/helpers/lemmas_helper.rb
+++ DDGLC/app/helpers/lemmas_helper.rb
@@ -6,7 +6,7 @@ module LemmasHelper
 
   def comment_button(target, label='')
     content_tag :button, class: 'btn btn-default btn-add-comment', type: 'button', 'data-toggle': 'collapse', 'data-target':target do
-      fa_icon('commenting') + label
+      fa_icon('comment-o') + label
     end
   end
 end
