1168f19b4776febc3bc696ab3289768a8a95e398
macksimiljan <macksimiljan@users.noreply.github.com>
2017-10-23 14:21:09 +0200

    finished sublemma view

--- DDGLC/app/controllers/sublemma_comments_controller.rb
+++ DDGLC/app/controllers/sublemma_comments_controller.rb
@@ -1,2 +1,31 @@
 class SublemmaCommentsController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+    @sublemma_comment = SublemmaComment.new
+  end
+
+  def create
+    @sublemma_comment = SublemmaComment.new(sublemma_comment_params)
+    @sublemma_comment.updated_by = current_user
+    @sublemma_comment.created_by = current_user
+    if @sublemma_comment.save
+      flash[:notice] = 'Comment saved.'
+    else
+      flash[:alert] = 'Comment could not be saved.'
+    end
+    redirect_to sublemma_path(sublemma_comment_params[:sublemma_id])
+  end
+
+  def destroy
+    @sublemma_comment = SublemmaComment.find(params[:id])
+    @sublemma_comment.delete
+    redirect_to sublemma_path(@sublemma_comment.sublemma)
+  end
+
+  private
+
+  def sublemma_comment_params
+    params.require(:sublemma_comment).permit(:content, :field, :sublemma_id)
+  end
 end
--- DDGLC/app/models/sublemma.rb
+++ DDGLC/app/models/sublemma.rb
@@ -5,8 +5,16 @@ class Sublemma < ApplicationRecord
   belongs_to :created_by, class_name: 'User'
   belongs_to :updated_by, class_name: 'User'
 
+  has_many :sublemma_comments
+
   paginates_per 15
 
   validates :label, presence: true
   validates :hierarchy, allow_blank: true, format: {with: /\A[IVX]+[.]?[A-Z]?[.]?[0-9]*\z/}
+
+  def comments_for(field)
+    comments = sublemma_comments.where(field: field)
+    comments ||= []
+    comments
+  end
 end
